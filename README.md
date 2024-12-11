b7c0c581e7c8b4bf5151a02bfe13b3cc958539e0
boydrhamilton <boydroberthamilton@gmail.com>
2024-12-11 11:12:32 -0800

    syntax

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# brh-learning 
+
+small ml experiments in rust using [penguins](https://www.kaggle.com/code/parulpandey/penguin-dataset-the-new-iris/input?select=penguins_size.csv)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 
-// https://www.kaggle.com/code/parulpandey/penguin-dataset-the-new-iris/input?select=penguins_size.csv
-
 use models::logisticregression::Model;
 use std::error::Error;
 use std::io;
@@ -14,16 +12,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut model: Model = models::logisticregression::Model::new(x, y);
     let weights: Vec<f64> = model.gradientdescent(0.1, 10000);
 
-    println!("{:?}\n", weights);
+    println!("Weights: {:?}\n", weights);
 
     // inputting new penguin data from user
     println!("INPUT: culmen_length_mm culmen_depth_mm flipper_length_mm body_mass_g");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("ruh roh");
 
-    let pengdata: Vec<f64> = input.split_whitespace()
-    .map(|s| s.parse().expect("nan"))
-    .collect();
+    let pengdata: Vec<f64> = input
+        .split_whitespace()
+        .map(|s| s.parse().expect("nan"))
+        .collect();
 
     let container: Vec<Vec<f64>> = vec![pengdata];
 
